f63c1f935534e08d3826678a2505555a5844f355
saud <saudtauqeer381@gmail.com>
2018-09-23 09:08:51 -0700

    includes a new public folder which has app.js, app.css, and index.html (server runs using these files), Components included for score and player.

---
+++ public/app.js
@@ -0,0 +1,34 @@
+const Header = () =>  {
+  return (
+    <header>
+      <h1>h1</h1>
+      <span className="stats">span</span>
+    </header>
+  );
+}
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement">-</button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment">+</button>
+    </div>
+  );
+}
+
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">saud</span>
+      <Counter />
+    </div>
+  );
+}
+
+
+  
+ReactDOM.render(
+  <Player />,
+  document.getElementById('root')
+);
