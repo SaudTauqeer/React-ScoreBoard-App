1ad17d3e189f41b2e3bf8d7a172b9ec55b5a1d3f
saud <saudtauqeer381@gmail.com>
2018-10-01 16:22:47 -0700

    moudles and doing state lifting, adding unidirectional data flow

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,10 @@
-import React from 'react';
-
-const Header = (props) => {
-  return (
-    <header>
-      <h1>{ props.title }</h1>
-      <span className="stats">Players: {props.totalPlayers}</span> 
-    </header>
-  );
-}
-
-class Counter extends React.Component {
-  state = { 
-    score: 0 
-  };
+import React, { Component } from 'react';
+import Header from './Header';
+import Player from './Player'
   
-  incrementScore = () => {
-    this.setState( prevState => ({
-      score: prevState.score + 1
-    }));
-  }
 
-  decrementScore = () => {
-    this.setState( prevState => ({
-      score: prevState.score - 1
-    }));
-  }
 
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-        <span className="counter-score">{ this.state.score }</span>
-        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
-      </div>
-    );
-  }
-}
-  
-const Player = (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-        { props.name }
-      </span>
-
-      <Counter />
-    </div>
-  );
-}
-
-class App extends React.Component {
+class App extends Component {
   state = {
     players: [
       {
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const Header = (props) => {
+    return (
+      <header>
+        <h1>{ props.title }</h1>
+        <span className="stats">Players: {props.totalPlayers}</span> 
+      </header>
+    );
+  }
+
+
+
+
+
+
+
+
+  export default Header;
