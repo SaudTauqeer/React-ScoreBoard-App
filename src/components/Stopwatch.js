d015d8fdf15719382e73d0cee411593263f503fc
saud <saudtauqeer381@gmail.com>
2018-10-09 18:20:16 -0700

    Added mount to stopwatch

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
 
 class Stopwatch extends Component {
+    state = {
+        isRunning: false,
+        elapsedTime: 0,
+        previousTime: 0
+    };
+
+    componentDidMount(){
+        this.intervalID = setInterval(()=> this.tick(), 100)
+    }
+
+
+
+    tick = () =>{
+        if (this.state.isRunning){
+            const now = Date.now();
+            this.setState({
+                previousTime: now,
+                elapsedTime: this.state.elapsedTime + (now - 
+                this.state.previousTime)
+            });
+        }
+    }
+
+    handleStopwatch = () => {
+        this.setState({ 
+            isRunning: !this.state.isRunning
+        });
+        if (!this.isRunning){
+            this.setState({
+                previousTime: Date.now()
+            });
+        }
+    }
+
+
     render(){
         return (
             <div className= "stopwatch" >
                 <h2>Stopwatch</h2>
                 <span className="stopwatch-time">0</span>
-                <button>Start</button>
+                <button onClick={ this.handleStopwatch }>
+                    { this.state.isRunning ? 'Stop' : 'Start' }
+                </button>
                 <button>Reset</button>
             </div>
         );
